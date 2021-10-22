d1d51d2c9c6444daf38ae79b429c762225d62b6f
qqwshst <qqws9875321@gmail.com>
2021-10-22 17:46:51 +0800

    新增 e狀態管理.cs
    加入enum 列舉
    聚會狀態
        0 可報名
        1 進行中
        2 已結束
    
    性別
        0 不公開
        1 男
        2 女
    
    權限
        0 會員
        1 管理者
    
    聚會垃圾桶
        0 顯示
        1 刪除
    
    參加者審核狀態
        0 未通過
        1 通過
    
    其他事項
    修復dbCookingModel.Context.cs

---
+++ prjCooking/Models/e狀態管理.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjCooking.Models
+{
+    public enum 聚會狀態
+    {
+        可報名 = 0,
+        進行中 = 1,
+        已結束 = 2
+    }
+
+    public enum 性別
+    {
+        不公開 = 0,
+        男 = 1,
+        女 = 2
+    }
+
+    public enum 權限
+    {
+        會員 = 0,
+        管理者 = 1
+    }
+
+    public enum 聚會垃圾桶
+    {
+        顯示 = 0,
+        刪除 = 1
+    }
+
+    public enum 參加者審核狀態
+    {
+        未通過 = 0,
+        通過 = 1
+    }
+}
