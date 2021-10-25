a65cec99973943d472bf960088a5a9e9967eece8
Hydrogen-bond <92904550+Hydrogen-bond@users.noreply.github.com>
2021-10-25 16:41:55 +0800

    新增首頁(未完)新增3筆聚會資料

--- prjCooking/Controllers/HomeController.cs
+++ prjCooking/Controllers/HomeController.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using prjCooking.Models;
+using System.Web.Security;
+
 
 namespace prjCooking.Controllers
 {
     public class HomeController : Controller
     {
+        dbCookingEntities db = new dbCookingEntities();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var parties = db.t聚會
+                .OrderByDescending(m => m.f聚會建立日期).ToList();
+
+
+            return View(parties);
         }
     }
 }
