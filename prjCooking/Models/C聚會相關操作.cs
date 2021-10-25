08b5d75500b5d1101fc322f57a4393a0d1fd9c52
qqwshst <qqws9875321@gmail.com>
2021-10-25 22:19:43 +0800

    修正 取消報名 取消活動 的查詢

--- prjCooking/Models/C聚會相關操作.cs
+++ prjCooking/Models/C聚會相關操作.cs
@@ -16,29 +16,19 @@ namespace prjCooking.Models
 
         public void 取消報名(int 會員Id, int 聚會Id)
         {
-            /*var tempList = (from 參加者 in _db.t參加者
-                       where 參加者.f會員Id == 會員Id.Value
-                       && 參加者.f聚會Id == 聚會Id
-                       select 參加者).FirstOrDefault();*/
-            List<t參加者> tempList = _db.t參加者.Where(m => m.f會員Id == 會員Id)
+            t參加者 取消報名的參加者 = _db.t參加者.Where(m => m.f會員Id == 會員Id)
                 .Where(m => m.f聚會Id == 聚會Id).FirstOrDefault();
 
-            foreach(t參加者 參加者 in tempList)
-            {
-                參加者.f報名 = true;
-            }
+            取消報名的參加者.f報名 = true;
 
             _db.SaveChanges();
         }
 
         public void 取消活動(int 聚會Id)
         {
-            List<t聚會> temp = _db.t聚會.Where(聚會 => 聚會.f聚會Id == 聚會Id).FirstOrDefault();
+            t聚會 取消的活動 = _db.t聚會.Where(聚會 => 聚會.f聚會Id == 聚會Id).FirstOrDefault();
 
-            foreach(t聚會 聚會 in temp)
-            {
-                聚會.f聚會垃圾桶 = (int)聚會垃圾桶.刪除;
-            }
+            取消的活動.f聚會垃圾桶 = Convert.ToBoolean(聚會垃圾桶.刪除);
 
             _db.SaveChanges();
         }
