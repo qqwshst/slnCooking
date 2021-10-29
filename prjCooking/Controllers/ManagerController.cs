3d96fc6e3ac113165da96aa8eea0fb55120bba33
fishfishla <92331599+fishfishla@users.noreply.github.com>
2021-10-29 18:19:57 +0800

    聚會管理、用戶管理更新

--- prjCooking/Controllers/ManagerController.cs
+++ prjCooking/Controllers/ManagerController.cs
@@ -14,6 +14,8 @@ namespace prjCooking.Controllers
         // GET: Manager
         public ActionResult PartyList()
         {
+
+
             IEnumerable<t聚會> datas = null;
             string keyword = Request.Form["txtKeyword"];
             if (string.IsNullOrEmpty(keyword))
@@ -22,25 +24,40 @@ namespace prjCooking.Controllers
                 datas = from p in (new dbCookingEntities()).t聚會 where p.f聚會名稱.Contains(keyword) select p;
 
             List<C管理者聚會ViewModel> Plist = new List<C管理者聚會ViewModel>();
+
+            //聚會資料放進viewmodel
             foreach (t聚會 p in datas)
+            {
                 Plist.Add(new C管理者聚會ViewModel() { party = p });
+
+            }
+            //修正部分viewmodel顯示資訊
+            foreach (C管理者聚會ViewModel p in Plist)
+            {
+                t會員 member = (new dbCookingEntities()).Cooking查詢某會員的資料By會員Id(p.f主辦人);
+
+                //建立日期轉換只顯示年月日
+                p.聚會建立日期 = Convert.ToDateTime(p.f聚會建立日期).ToShortDateString();
+
+                p.煮辦人姓名 = member.f會員姓名;
+
+                string party狀態;
+                if (p.f聚會狀態 == Convert.ToInt32(聚會狀態.可報名))
+                    party狀態 = "可報名";
+                else if (p.f聚會狀態 == Convert.ToInt32(聚會狀態.進行中))
+                    party狀態 = "進行中";
+                else
+                    party狀態 = "已結束";
+                p.聚會狀態 = party狀態;
+                if (p.f聚會垃圾桶 == false)
+                    p.聚會垃圾桶 = " ";
+                else
+                    p.聚會垃圾桶 = "✘";
+
+            }
+
+
             return View(Plist);
-        }
-        public ActionResult List()
-        {
-            IEnumerable<t會員> datas = null;
-            string keyword = Request.Form["txtKeyword"];
-            if (string.IsNullOrEmpty(keyword))
-                datas = from p in (new dbCookingEntities()).t會員 select p;
-            else
-                datas = from p in (new dbCookingEntities()).t會員 where p.f會員姓名.Contains(keyword)||p.f會員信箱.Contains(keyword) select p;
-
-
-            List<C管理者會員ViewModel> list = new List<C管理者會員ViewModel>();
-            foreach (t會員 p in datas)
-                list.Add(new C管理者會員ViewModel() { member = p });
-            return View(list);
-
         }
         public ActionResult Details(int id)
         {
@@ -63,6 +80,22 @@ namespace prjCooking.Controllers
                 db.SaveChanges();
             }
             return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            IEnumerable<t會員> datas = null;
+            string keyword = Request.Form["txtKeyword"];
+            if (string.IsNullOrEmpty(keyword))
+                datas = from p in (new dbCookingEntities()).t會員 select p;
+            else
+                datas = from p in (new dbCookingEntities()).t會員 where p.f會員姓名.Contains(keyword) || p.f會員信箱.Contains(keyword) select p;
+
+
+            List<C管理者會員ViewModel> list = new List<C管理者會員ViewModel>();
+            foreach (t會員 p in datas)
+                list.Add(new C管理者會員ViewModel() { member = p });
+            return View(list);
+
         }
     }
 }
--- prjCooking/ViewModel/C管理者聚會ViewModel.cs
+++ prjCooking/ViewModel/C管理者聚會ViewModel.cs
@@ -11,50 +11,45 @@ namespace prjCooking.ViewModel
     public class C管理者聚會ViewModel
     {
         public t聚會 party { get; set; } = null;
-      
-       
+
+
         public C管理者聚會ViewModel()
-        { 
+        {
             this.party = new t聚會();
         }
-        
-        public int f聚會Id 
+
+        public int f聚會Id
         {
             get { return this.party.f聚會Id; }
             set { this.party.f聚會Id = value; }
         }
-        [DisplayName("煮辦人")]
-        public int f主辦人
-        {
-            get { return this.party.f主辦人; }
-            set { this.party.f主辦人 = value; }
-        }
+
         [DisplayName("聚會名稱")]
-        public string f聚會名稱 
+        public string f聚會名稱
         {
             get { return this.party.f聚會名稱; }
             set { this.party.f聚會名稱 = value; }
         }
         [DisplayName("內容")]
-        public string f聚會內容 
+        public string f聚會內容
         {
             get { return this.party.f聚會內容; }
             set { this.party.f聚會內容 = value; }
         }
-        
-        public string f聚會照片 
+
+        public string f聚會照片
         {
             get { return this.party.f聚會照片; }
             set { this.party.f聚會照片 = value; }
         }
         [DisplayName("日期")]
-        public System.DateTime f聚會日期 
+        public System.DateTime f聚會日期
         {
             get { return this.party.f聚會日期; }
             set { this.party.f聚會日期 = value; }
         }
         [DisplayName("開始時間")]
-        public System.DateTime f聚會開始時間 
+        public System.DateTime f聚會開始時間
         {
             get { return this.party.f聚會開始時間; }
             set { this.party.f聚會開始時間 = value; }
@@ -66,7 +61,7 @@ namespace prjCooking.ViewModel
             set { this.party.f聚會結束時間 = value; }
         }
         [DisplayName("名額")]
-        public Nullable<int> f名額 
+        public Nullable<int> f名額
         {
             get { return this.party.f名額; }
             set { this.party.f名額 = value; }
@@ -78,19 +73,19 @@ namespace prjCooking.ViewModel
             set { this.party.f聚會軟體 = value; }
         }
         [DisplayName("連結")]
-        public string f聚會軟體URL 
+        public string f聚會軟體URL
         {
             get { return this.party.f聚會軟體URL; }
             set { this.party.f聚會軟體URL = value; }
         }
         [DisplayName("關鍵字")]
-        public string f聚會關鍵字 
+        public string f聚會關鍵字
         {
             get { return this.party.f聚會關鍵字; }
             set { this.party.f聚會關鍵字 = value; }
         }
         [DisplayName("通訊軟體")]
-        public string f聚會通訊軟體 
+        public string f聚會通訊軟體
         {
             get { return this.party.f聚會通訊軟體; }
             set { this.party.f聚會通訊軟體 = value; }
@@ -101,24 +96,45 @@ namespace prjCooking.ViewModel
             get { return this.party.f聚會通訊軟體帳號; }
             set { this.party.f聚會通訊軟體帳號 = value; }
         }
-        
         public Nullable<int> f聚會狀態
         {
             get { return this.party.f聚會狀態; }
             set { this.party.f聚會狀態 = value; }
         }
-        
-        public Nullable<bool> f聚會垃圾桶 
+        public Nullable<bool> f聚會垃圾桶
         {
             get { return this.party.f聚會垃圾桶; }
             set { this.party.f聚會垃圾桶 = value; }
         }
-        
-        public System.DateTime f聚會建立日期 
+
+
+
+        public string 聚會狀態
+        {
+            get; set;
+        }
+        [DisplayName(" ")]
+        public string 聚會垃圾桶
+        {
+            get; set;
+        }
+
+        public System.DateTime f聚會建立日期
         {
             get { return this.party.f聚會建立日期; }
             set { this.party.f聚會建立日期 = value; }
         }
-
+        [DisplayName("聚會建立日期")]
+        public string 聚會建立日期
+        { get; set; }
+        [DisplayName("煮辦人")]
+        public int f主辦人
+        {
+            get { return this.party.f主辦人; }
+            set { this.party.f主辦人 = value; }
+        }
+        [DisplayName("煮辦人姓名")]
+        public string 煮辦人姓名
+        { get; set; }
     }
 }
