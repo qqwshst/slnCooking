91311031f216b9308fe58c5bbfc5bbd435681a18
fishfishla <92331599+fishfishla@users.noreply.github.com>
2021-10-30 10:17:54 +0800

    修正show個人頁面程式碼

--- prjCooking/Controllers/MemberController.cs
+++ prjCooking/Controllers/MemberController.cs
@@ -51,9 +51,9 @@ namespace prjCooking.Controllers
             return RedirectToAction("Show個人頁面");
         }
 
-        public ActionResult Show個人頁面(int? id = 0)
+        public ActionResult Show個人頁面(int? id)
         {
-            id = ((t會員)Session[CSessionKey.登入會員_t會員]).f會員Id;
+           
             t會員 member_select = (new dbCookingEntities()).Cooking查詢某會員的資料By會員Id(id);
          
             return View(new CAllMember() { member = member_select });
