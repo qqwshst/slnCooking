16635089707bc9bdab95627a6885e679411a73a8
qqwshst <qqws9875321@gmail.com>
2021-11-01 14:26:11 +0800

    修改主頁面
    可以撈取所有聚會
    
    進行中、未開始、已結束 未實作

--- prjCooking/Controllers/HomeController.cs
+++ prjCooking/Controllers/HomeController.cs
@@ -5,22 +5,21 @@ using System.Web;
 using System.Web.Mvc;
 using prjCooking.Models;
 using System.Web.Security;
-
+using prjCooking.ViewModel;
 
 namespace prjCooking.Controllers
 {
     public class HomeController : Controller
     {
-        dbCookingEntities db = new dbCookingEntities();
-
         // GET: Home
         public ActionResult Index()
         {
-            var parties = db.t聚會
-                .OrderByDescending(m => m.f聚會建立日期).ToList();
+            C主頁面撈取 撈取 = new C主頁面撈取();
+            C主頁面展示ViewModel vmodel = new C主頁面展示ViewModel();
+            vmodel.全部聚會 = 撈取.所有聚會();
 
 
-            return View(parties);
+            return View(vmodel);
         }
     }
 }
---
+++ prjCooking/Models/C主頁面撈取.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjCooking.Models
+{
+    public class C主頁面撈取
+    {
+        private dbCookingEntities db;
+
+        public C主頁面撈取()
+        {
+            db = new dbCookingEntities();
+        }
+
+        public List<t聚會> 所有聚會()
+        {
+            List<t聚會> 聚會List = db.Cooking查詢所有沒被刪除聚會List();
+            聚會List.Reverse();
+
+            return 聚會List;
+        }
+
+
+    }
+}
