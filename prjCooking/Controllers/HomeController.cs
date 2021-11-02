8cc911db1256fbb0204a1db7dd37688eb3d3c1a3
Hydrogen-bond <92904550+Hydrogen-bond@users.noreply.github.com>
2021-11-02 15:09:44 +0800

    加入導覽列關鍵字搜尋功能

--- prjCooking/Controllers/HomeController.cs
+++ prjCooking/Controllers/HomeController.cs
@@ -12,21 +12,16 @@ namespace prjCooking.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(string txtSearch)
         {
             C聚會狀態更新.Update();
-            C主頁面撈取 撈取 = new C主頁面撈取();
+            C主頁面撈取 撈取 = new C主頁面撈取(txtSearch);
             C主頁面展示ViewModel vmodel = new C主頁面展示ViewModel();
             vmodel.全部聚會 = 撈取.所有聚會();
             vmodel.進行中聚會 = 撈取.進行中聚會();
             vmodel.未開始聚會 = 撈取.未開始聚會();
             vmodel.已結束聚會 = 撈取.已結束聚會();
             return View(vmodel);
-        }
-
-        public ActionResult Search(string txtSearch)
-        {
-            return View();
         }
     }
 }
--- prjCooking/Models/C主頁面撈取.cs
+++ prjCooking/Models/C主頁面撈取.cs
@@ -7,32 +7,47 @@ namespace prjCooking.Models
 {
     public class C主頁面撈取
     {
-        private dbCookingEntities db;
+        private List<t聚會> _聚會List;
+        private string _keyword;
 
-        public C主頁面撈取()
+
+        public C主頁面撈取(string keyword)
         {
-            db = new dbCookingEntities();
+            dbCookingEntities db = new dbCookingEntities();
+            _聚會List = db.Cooking查詢所有沒被刪除聚會List();
+            _聚會List.Reverse();
+            _keyword = keyword;
+
         }
 
         public List<t聚會> 所有聚會()
         {
-            List<t聚會> 聚會List = db.Cooking查詢所有沒被刪除聚會List();
-            聚會List.Reverse();
+            List<t聚會> 所有聚會List = new List<t聚會>();
+            所有聚會List.AddRange(_聚會List);
 
-            return 聚會List;
+
+            if (!string.IsNullOrEmpty(_keyword))
+            {
+                所有聚會List = 尋找關鍵字聚會(_聚會List);
+            }
+
+            return 所有聚會List;
         }
+
 
         public List<t聚會> 進行中聚會()
         {
-            List<t聚會> 聚會List = db.Cooking查詢所有沒被刪除聚會List();
             List<t聚會> 進行中聚會List = new List<t聚會>();
 
-            聚會List.Reverse();
-
-            foreach (t聚會 聚會 in 聚會List)
+            foreach(t聚會 聚會 in _聚會List)
             {
                 if (聚會.f聚會狀態.Value == (int)聚會狀態.進行中)
                     進行中聚會List.Add(聚會);
+            }
+
+            if (!string.IsNullOrEmpty(_keyword))
+            {
+                進行中聚會List = 尋找關鍵字聚會(進行中聚會List);
             }
 
             return 進行中聚會List;
@@ -40,15 +55,17 @@ namespace prjCooking.Models
 
         public List<t聚會> 未開始聚會()
         {
-            List<t聚會> 聚會List = db.Cooking查詢所有沒被刪除聚會List();
             List<t聚會> 未開始聚會List = new List<t聚會>();
 
-            聚會List.Reverse();
-
-            foreach(t聚會 聚會 in 聚會List)
+            foreach (t聚會 聚會 in _聚會List)
             {
                 if (聚會.f聚會狀態.Value == (int)聚會狀態.可報名)
                     未開始聚會List.Add(聚會);
+            }
+
+            if (!string.IsNullOrEmpty(_keyword))
+            {
+                未開始聚會List = 尋找關鍵字聚會(未開始聚會List);
             }
 
             return 未開始聚會List;
@@ -56,18 +73,32 @@ namespace prjCooking.Models
 
         public List<t聚會> 已結束聚會()
         {
-            List<t聚會> 聚會List = db.Cooking查詢所有沒被刪除聚會List();
             List<t聚會> 已結束聚會List = new List<t聚會>();
 
-            聚會List.Reverse();
-
-            foreach (t聚會 聚會 in 聚會List)
+            foreach (t聚會 聚會 in _聚會List)
             {
                 if (聚會.f聚會狀態.Value == (int)聚會狀態.已結束)
                     已結束聚會List.Add(聚會);
             }
 
+            if (!string.IsNullOrEmpty(_keyword))
+            {
+                已結束聚會List = 尋找關鍵字聚會(已結束聚會List);
+            }
+
             return 已結束聚會List;
+        }
+
+        private List<t聚會> 尋找關鍵字聚會(List<t聚會> listTemp)
+        {
+            List<t聚會> 關鍵字聚會List = new List<t聚會>();
+            foreach(t聚會 聚會 in listTemp)
+            {
+                if (聚會.f聚會關鍵字.Contains(_keyword) || 聚會.f聚會名稱.Contains(_keyword) || 聚會.f聚會內容.Contains(_keyword))
+                    關鍵字聚會List.Add(聚會);
+            }
+            return 關鍵字聚會List;
+
         }
     }
 }
