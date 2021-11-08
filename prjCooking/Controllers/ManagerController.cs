ace535485aa077f309337d5e8f20362b6f060e69
fishfishla <92331599+fishfishla@users.noreply.github.com>
2021-11-08 14:54:43 +0800

    檢舉應該ok了

--- prjCooking/Controllers/ManagerController.cs
+++ prjCooking/Controllers/ManagerController.cs
@@ -10,6 +10,74 @@ namespace prjCooking.Controllers
 {
     public class ManagerController : Controller
     {
+        public ActionResult query檢舉()
+        {
+            dbCookingEntities db = new dbCookingEntities();
+            var query檢舉 = (from prod in db.t檢舉
+                           select prod.f被檢舉的聚會Id).Distinct().ToList();
+
+            IEnumerable<t聚會> datas = null;
+            int id = 0;
+            List<C管理者聚會ViewModel> Plist = new List<C管理者聚會ViewModel>();
+
+            foreach (int m in query檢舉)
+            {
+                //m.f被檢舉的聚會Id = id;
+                datas = (from p in (new dbCookingEntities()).t聚會 where p.f聚會Id == m select p);
+                //聚會資料放進viewmodel
+                foreach (t聚會 p in datas)
+                {
+                    Plist.Add(new C管理者聚會ViewModel() { party = p });
+
+                }
+            }
+
+            //修正部分viewmodel顯示資訊
+            foreach (C管理者聚會ViewModel p in Plist)
+            {
+                t會員 member = (new dbCookingEntities()).Cooking查詢某會員的資料By會員Id(p.f主辦人);
+
+                t檢舉 query檢舉p = new t檢舉();
+                query檢舉p = db.t檢舉.FirstOrDefault(m => m.f被檢舉的聚會Id == p.f聚會Id);
+
+                p.聚會建立日期 = Convert.ToDateTime(p.f聚會建立日期).ToShortDateString();
+
+                p.煮辦人姓名 = member.f會員姓名;
+
+                string party狀態;
+                if (p.f聚會狀態 == Convert.ToInt32(聚會狀態.可報名))
+                    party狀態 = "可報名";
+                else if (p.f聚會狀態 == Convert.ToInt32(聚會狀態.進行中))
+                    party狀態 = "進行中";
+                else
+                    party狀態 = "已結束";
+                p.聚會狀態 = party狀態;
+                if (p.f聚會垃圾桶 == false)
+                    p.聚會垃圾桶 = " ";
+                else
+                    p.聚會垃圾桶 = "✘";
+
+                if (query檢舉p != null)
+                {
+
+                    if (query檢舉p.f檢舉原因 == "已檢舉")
+                    {
+                        p.聚會檢舉狀態顯示 = "已檢舉";
+                    }
+                    else
+                    {
+                        p.聚會檢舉狀態顯示 = " ";
+
+                    }
+                }
+
+
+
+            }
+
+
+            return View(Plist);
+        }
         public ActionResult 檢舉頁面(int? id)
         {
             ViewBag.f聚會id = id;
@@ -37,7 +105,7 @@ namespace prjCooking.Controllers
                 db.t檢舉.Remove(p);
                 db.SaveChanges();
             }
-           
+
 
             return RedirectToAction("PartyList");
 
@@ -49,14 +117,14 @@ namespace prjCooking.Controllers
                            where prod.f被檢舉的聚會Id == id
                            select prod).ToList();
 
-           
+
             foreach (t檢舉 p in query檢舉)
             {
                 //p.f檢舉原因 = DateTime.Today.ToString()+"已處理";
                 db.t檢舉.Remove(p);
                 db.SaveChanges();
             }
-            
+
             return RedirectToAction("PartyList");
 
         }
